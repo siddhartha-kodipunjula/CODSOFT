5f5ab319c82122ab3ff57a44a28d5c88679997e8
Siddhartha <kodipunjulasiddu27@gmail.com>
2026-06-04 11:55:10 +0530

    Implemented student management operations

---
+++ Student Management System/src/StudentManager.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class StudentManager {
+
+    private ArrayList<Student> students;
+
+    public StudentManager() {
+
+        students = new ArrayList<>();
+    }
+
+    public void addStudent(Student student) {
+
+        students.add(student);
+
+        System.out.println(
+                "Student Added Successfully!");
+    }
+
+    public void displayStudents() {
+
+        if (students.isEmpty()) {
+
+            System.out.println(
+                    "No Student Records Found");
+
+            return;
+        }
+
+        System.out.println(
+                "\nID | Name | Age | Course | Grade");
+
+        for (Student student : students) {
+
+            student.displayStudent();
+        }
+    }
+
+    public ArrayList<Student> getStudents() {
+
+        return students;
+    }
+}
