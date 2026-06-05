1849785210925297e924d4f0544de794dd970970
Siddhartha <kodipunjulasiddu27@gmail.com>
2026-06-05 11:08:13 +0530

    Implemented random number generator

---
+++ Number Guessing Game/src/Game.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Game {
+
+    private int targetNumber;
+
+    public Game() {
+
+        Random random = new Random();
+        targetNumber = random.nextInt(100) + 1;
+    }
+
+    public int getTargetNumber() {
+        return targetNumber;
+    }
+}
